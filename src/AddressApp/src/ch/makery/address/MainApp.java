ede697b5ab446244af23a7d0f3e7b84e0de8727a
qurter76 <59903968+qurter76@users.noreply.github.com>
2020-05-11 22:08:38 +0900

    AddressApp

--- src/AddressApp/src/ch/makery/address/MainApp.java
+++ src/AddressApp/src/ch/makery/address/MainApp.java
@@ -2,7 +2,11 @@ package ch.makery.address;
 
 import java.io.IOException;
 
+import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -10,6 +14,36 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
+	
+	/**
+	 * 연락처에 대한 observable 리스트
+	 */
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+	/**
+	 * 생성자
+	 */
+	public MainApp() {
+		// 샘플 데이터를 추가한다
+		personData.add(new Person("Hans", "Muster"));
+		personData.add(new Person("Ruth", "Mueller"));
+		personData.add(new Person("Heinz", "Kurz"));
+		personData.add(new Person("Cornelia", "Meier"));
+		personData.add(new Person("Werner", "Meyer"));
+		personData.add(new Person("Lydia", "Kunz"));
+		personData.add(new Person("Anna", "Best"));
+		personData.add(new Person("Stefan", "Meier"));
+		personData.add(new Person("Martin", "Mueller"));
+	}
+
+	/**
+	 * 연락처에 대한 observable 리스트를 반환한다.
+	 * @return
+	 */
+	public ObservableList<Person> getPersonData() {
+		return personData;
+	}
+
 
     private Stage primaryStage;
     private BorderPane rootLayout;
@@ -43,22 +77,28 @@ public class MainApp extends Application {
         }
     }
 
-    /**
-     * 상위 레이아웃 안에 연락처 요약(person overview)을 보여준다.
-     */
-    public void showPersonOverview() {
-        try {
-            // 연락처 요약을 가져온다.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
-            AnchorPane personOverview = (AnchorPane) loader.load();
+	
+	/**
+	 * 상위 레이아웃 안에 연락처 요약(person overview)을 보여준다.
+	 */
+	public void showPersonOverview() {
+	    try {
+	        // 연락처 요약을 가져온다.
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+	        AnchorPane personOverview = (AnchorPane) loader.load();
 
-            // 연락처 요약을 상위 레이아웃 가운데로 설정한다.
-            rootLayout.setCenter(personOverview);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	        // 연락처 요약을 상위 레이아웃 가운데로 설정한다.
+	        rootLayout.setCenter(personOverview);
+
+	        // 메인 애플리케이션이 컨트롤러를 이용할 수 있게 한다.
+	        PersonOverviewController controller = loader.getController();
+	        controller.setMainApp(this);
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
 
 	/**
 	 * 메인 스테이지를 반환한다.
